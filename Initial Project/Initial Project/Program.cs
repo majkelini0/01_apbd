b8a44b15f440d1f401baea81e326d67384801dee
s27799 <s27799@pjwstk.edu.pl>
2024-03-14 15:37:19 +0100

    Mod1 commit

--- Initial Project/Initial Project/Program.cs
+++ Initial Project/Initial Project/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Security.AccessControl;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Pierwszy commit");
